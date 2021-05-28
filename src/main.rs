68561009203427a57987a92a6a45dc0241260475
Sebastian <xxsebastian.klaehnxx@gmail.com>
2021-05-28 11:50:34 +0200

    use perlin noise

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use bevy::{pbr::AmbientLight, prelude::*};
 use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
-use map::{TreeBuilderBuilder, TreeBuilderConfig};
+use map::trees::{TreeBuilderBuilder, Config};
+use noise::{NoiseFn, Perlin, Seedable};
 mod map;
 
 fn main() {
     App::build()
         .insert_resource(AmbientLight {
             color: Color::WHITE,
-            brightness: 1.0 / 5.0f32,
+            brightness: 1.0 / 2.0f32,
         })
         .insert_resource(Msaa { samples: 4 })
         .add_startup_system(setup.system())
@@ -22,26 +23,30 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let tree_builder = TreeBuilderBuilder::new()
-        .with_config(TreeBuilderConfig {
+        .with_config(Config {
             tile_height: 10.0,
             ..Default::default()
         })
         .with_levels(20)
         .build(&mut meshes, &mut materials);
 
-    let mapgen = map::generators::DonutGenerator::new(100, 100, 20, 15.0);
-
+    let perlin = Perlin::new();
+    perlin.set_seed(3546);
+    println!("perlin: {}", perlin.get([1.1234, 1.123005001]));
     for x in -50..50 {
         for z in -50..50 {
             if x % 2 == 0 && z % 2 == 0 {
-                let size = mapgen.compute(x, z);
-                tree_builder.build_tree_at(x as f32, z as f32, size, &mut commands);
+                let size = perlin.get([(x + 50) as f64 / 20.1, (z + 50) as f64 / 20.1]) * 8.0;
+                if size > 2.0 {
+
+                    tree_builder.build_tree_at(x as f32, z as f32, size as f32, &mut commands);
+                }
             }
         }
     }
     commands
         .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(30.0, 30.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         })
         .insert(FlyCamera::default());
